059ccef93c302992d2e0d49f7368526d159e7a11
Ahmed Alaa <me.ahmedalaa@gmail.com>
2024-07-14 07:59:55 +0300

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,12 +6,12 @@ const Dashboard = () => {
   const [categoryCount, setCategoryCount] = useState(0);
 
   useEffect(() => {
-    // Fetch products count
+    
     axios.get('https://fakestoreapi.com/products')
       .then(response => setProductCount(response.data.length))
       .catch(error => console.error(error));
 
-    // Fetch categories count
+    
     axios.get('https://fakestoreapi.com/products/categories')
       .then(response => setCategoryCount(response.data.length))
       .catch(error => console.error(error));
